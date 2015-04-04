364b655a1aa01048c82e6af270b671741fa02cce
Chris Barry <cwb@outlook.com>
2015-04-04 16:10:07 -0400

    debuggg

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ Bot.prototype.serve = function(myServer) {
   myServer.addPath(this.tail);
   myServer.on('serverMessage', function(s) {
   	console.log("Bot received message");
+  	console.log("Message: " JSON.stringify(s));
     if (s.botName == self.name) {
       console.log('got message from' + s.botName);
       self.emit('botMessage', self, s);
